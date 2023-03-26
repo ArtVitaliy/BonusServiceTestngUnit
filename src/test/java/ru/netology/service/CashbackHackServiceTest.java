ef87cb2af581fd55fdf82bb2e94f8d87b546efeb
ArtVitaly <0331116@mail.ru>
2023-03-26 20:15:09 +0300

    fixed tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,9 +7,9 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldCalculateIfAmount100() {
-        int amount = 100;
-        int expected = 900;
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int expected = 0;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
@@ -28,21 +28,11 @@ public class CashbackHackServiceTest {
 
     }
 
-    @Test
-    public void shouldCalculateIfAmount1000() {
-        int amount = 1000;
-        int expected = 0;
-
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
-        assertEquals(actual, expected);
-
-    }
 
     @Test
     public void shouldCalculateIfAmount1500() {
         int amount = 1500;
-        int expected = 0;
+        int expected = 500;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
